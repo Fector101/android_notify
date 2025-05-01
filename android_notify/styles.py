45037b3d8cbcc91e3b33c3dc690ec375c2c6058c
fector101 <fabianjoseph063@gmail.com>
2025-05-01 14:39:40 +0100

    v1.59 parsing out NotificationStyles

--- android_notify/styles.py
+++ android_notify/styles.py
@@ -6,8 +6,14 @@ class NotificationStyles:
 
     PROGRESS = "progress"
     INBOX = "inbox"
+    
+    # v1.59+
+    # Deprecated
+    # setBigText == Notification(...,big_picture_path="...",style=NotificationStyles.BIG_TEXT)
+    # setLargeIcon == Notification(...,large_icon_path="...",style=NotificationStyles.LARGE_ICON)
+    # setBigPicture == Notification(...,body="...",style=NotificationStyles.BIG_PICTURE)
+    # Use .refresh to apply any new changes after .send
     BIG_TEXT = "big_text"
-
     LARGE_ICON = "large_icon"
     BIG_PICTURE = "big_picture"
     BOTH_IMGS = "both_imgs"
