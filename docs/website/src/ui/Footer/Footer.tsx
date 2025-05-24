a41c24354aebb689467489007ecc81832ae354ae
fector101 <fabianjoseph063@gmail.com>
2025-05-24 20:23:25 +0100

    better footer layout

--- docs/website/src/ui/Footer/Footer.tsx
+++ docs/website/src/ui/Footer/Footer.tsx
@@ -11,25 +11,28 @@ export default function Footer(): JSX.Element {
     const [date, setDate] = useState('');
 
     useEffect(() => {
-      fetch('/last-updated.txt')
-        .then(res => res.text())
-        .then(text => setDate(text));
+        fetch('/last-updated.txt')
+            .then(res => res.text())
+            .then(text => setDate(text));
     }, []);
     return (
-        <footer className='flex fd-column justify-content-cen text-align-center footer'>
+        <footer className='flex justify-content-cen text-align-center align-items-cen'>
             {/* <h2 className='footer-title'>Laner Documentation</h2>
             <p className='footer-subtitle'>A comprehensive guide to using Laner</p> */}
             {/* <p>Made with ❤️ using <Link to='https://react.dev/'>React</Link> and <Link to='https://vitejs.dev/'>Vite</Link></p> */}
             {/* <p>Documentation hosted on <Link to='https://vercel.com/'>Vercel</Link></p> */}
-
-            <p>Thoroughly Tested and supported by <Link to='https://github.com/Fector101/laner'>Laner</Link> </p>
-            <div className='support-links-box flex fd-column justify-content-cen text-align-center'>
-            <p>Find this project helpful?</p>
-            <p> consider buying me a <Link to='https://www.buymeacoffee.com/fector101' className="buy-me-a-coffee">Coffee ☕️</Link></p>
-            <p> Or a star on 🌟 <Link to='https://github.com/Fector101/android_notify'>GitHub.</Link></p>
-            <p>Your support helps maintain and improve the project.</p>
+            <div className="flex fd-column gap-10">
+                <p>Thoroughly Tested and supported by <Link to='https://github.com/Fector101/laner'>Laner</Link> </p>
+                <p>Last Updated: {date}, © 2023 Fector101</p>
             </div>
-            <p>Last Updated: {date}, © 2023 Fector101</p>
+
+            <div className='support-links-box flex fd-column justify-content-cen text-align-center'>
+                <p>Find this project helpful?</p>
+                <p> consider buying me a <Link to='https://www.buymeacoffee.com/fector101' className="buy-me-a-coffee">Coffee ☕️</Link></p>
+                <p> Or a star on 🌟 <Link to='https://github.com/Fector101/android_notify'>GitHub.</Link></p>
+                <p>Your support helps maintain and improve the project.</p>
+            </div>
+
         </footer>
     )
 }
