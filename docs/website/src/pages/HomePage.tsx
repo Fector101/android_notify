919a96f53a63dce9f5a91d1b20d10ca8d2f74145
Fabian <fabianjoseph063@gmail.com>
2025-05-25 16:32:27 +0100

    Update HomePage.tsx
    fixed mismatch Argos in docs.

--- docs/website/src/pages/HomePage.tsx
+++ docs/website/src/pages/HomePage.tsx
@@ -19,8 +19,8 @@ export default function HomePage() {
                 <ul className='inner-section-2 paragraph'>
                     <li><span className="code green-shade">setSmallIcon</span> == <span className="code yellow-shade">Notification(...,app_icon="...") </span></li>
                     <li><span className="code green-shade">setLargeIcon</span> == <span className="code yellow-shade">Notification(...,large_icon_path="...",style=NotificationStyles.LARGE_ICON)</span></li>
-                    <li><span className="code green-shade">setBigPicture</span> == <span className="code yellow-shade">Notification(...,body="...",style=NotificationStyles.BIG_PICTURE)</span></li>
-                    <li><span className="code green-shade">setBigText</span> == <span className="code yellow-shade">Notification(...,big_picture_path="...",style=NotificationStyles.BIG_TEXT)</span></li>
+                    <li><span className="code green-shade">setBigPicture</span> == <span className="code yellow-shade">Notification(...,big_picture_path="...",style=NotificationStyles.BIG_PICTURE)</span></li>
+                    <li><span className="code green-shade">setBigText</span> == <span className="code yellow-shade">Notification(...,body="...",style=NotificationStyles.BIG_TEXT)</span></li>
                 </ul>
                 <h3 className='paragraph'>New Features</h3>
                 <span className='code width100per flex paragraph'></span>
