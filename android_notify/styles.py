f597d5ee48b5796d78f203afe88ee1cab1162229
fector101 <fabianjoseph063@gmail.com>
2025-06-02 02:56:09 +0100

    removed unless file

--- android_notify/styles.py
+++ android_notify/styles.py
@@ -2,17 +2,20 @@
 
 class NotificationStyles:
     """ Safely Adding Styles"""
-    DEFAULT = "simple"
-
-    PROGRESS = "progress"
-    INBOX = "inbox"
-    
+        
     # v1.59+
     # Deprecated
     # setBigText == Notification(...,big_picture_path="...",style=NotificationStyles.BIG_TEXT)
     # setLargeIcon == Notification(...,large_icon_path="...",style=NotificationStyles.LARGE_ICON)
     # setBigPicture == Notification(...,body="...",style=NotificationStyles.BIG_PICTURE)
+    # setLines == Notification(...,lines_txt="...",style=NotificationStyles.INBOX)
+    # Set progress_current_value and progress_max_value for progress style
+
     # Use .refresh to apply any new changes after .send
+    
+    DEFAULT = "simple"
+    PROGRESS = "progress"
+    INBOX = "inbox"
     BIG_TEXT = "big_text"
     LARGE_ICON = "large_icon"
     BIG_PICTURE = "big_picture"
