72fa6ccc6c4b9669ebdabdad5197245f267885e7
Fabian <fabianjoseph063@gmail.com>
2025-11-16 18:12:41 +0100

    refactor: removed unused import

--- android_notify/sword.py
+++ android_notify/sword.py
@@ -6,7 +6,7 @@ from .config import cast, autoclass
 from .an_types import Importance
 from .an_utils import can_accept_arguments, get_python_activity_context, \
     get_android_importance, generate_channel_id, get_img_from_path, setLayoutText, \
-    get_bitmap_from_url, add_data_to_intent, get_sound_uri, get_flet_fallback_icon_path, get_bitmap_from_path,get_package_path
+    get_bitmap_from_url, add_data_to_intent, get_sound_uri, get_flet_fallback_icon_path, get_bitmap_from_path
 
 from .config import from_service_file, get_python_activity,get_notification_manager,ON_ANDROID,on_flet_app
 from .config import (Bundle, String, BuildVersion,
