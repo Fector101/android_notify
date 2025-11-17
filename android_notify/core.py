0cb0561dc869b1db0a04a1b2ed75c251449d988b
Fabian <fabianjoseph063@gmail.com>
2025-11-17 08:42:31 +0100

    refactor: hard coded api level

--- android_notify/core.py
+++ android_notify/core.py
@@ -66,7 +66,7 @@ def asks_permission_if_needed():
         Manifest = autoclass('android.Manifest$permission')
         VERSION_CODES = autoclass('android.os.Build$VERSION_CODES')
 
-        if BuildVersion.SDK_INT >= VERSION_CODES.TIRAMISU:
+        if BuildVersion.SDK_INT >= 33:
             permission = Manifest.POST_NOTIFICATIONS
             granted = ContextCompat.checkSelfPermission(context, permission)
 
