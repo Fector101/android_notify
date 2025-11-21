c519b0d82c86ee99c9c1a0e291a5fc7dab4e7bd5
Fabian <fabianjoseph063@gmail.com>
2025-11-21 21:47:52 +0100

    feat: Implement setWhen method for notification timestamp (#10)
    * feat: Implement setWhen method for notification timestamp
    
    Adds a method to set notification timestamp in the past.
    
    * Add Android check for notification timestamp
    
    Add conditional check for Android before setting notification time.

--- android_notify/sword.py
+++ android_notify/sword.py
@@ -290,6 +290,28 @@ class Notification(BaseNotification):
         if ON_ANDROID:
             self.__builder.setColor(Color.parseColor(color))
 
+    def setWhen(self, secs_ago):
+        """
+        Sets the notification's timestamp to a specified number of seconds in the past.
+
+        Parameters
+        ----------
+        secs_ago : int or float
+            The number of seconds ago the notification should appear to have been posted.
+            For example, `60` means "1 minute ago", `3600` means "1 hour ago".
+
+        Notes
+        -----
+        - Android expects the `when` timestamp in **milliseconds** since the Unix epoch.
+        """
+        
+        if ON_ANDROID:
+            ms=int((time.time() - secs_ago) * 1000)
+            self.__builder.setWhen(ms)
+            self.__builder.setShowWhen(True)
+        if self.logs:
+            print(f"Done setting secs ago {secs_ago}")
+
     def showInfiniteProgressBar(self):
         """Displays an (Infinite) progress Bar in Notification, that continues loading indefinitely.
         Can be Removed By `removeProgressBar` Method
