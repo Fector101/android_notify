ff90e23f89c6712357b5a802d3e0b99519e02776
fabian <fabianjoseph063@gmail.com>
2026-01-18 20:16:04 +0100

    testing with no ui

--- android_notify/tests/base_test.py
+++ android_notify/tests/base_test.py
@@ -14,3 +14,10 @@ class AndroidNotifyBaseTest(unittest.TestCase):
 
     def tearDown(self):
         time.sleep(secs3)
+
+from android_notify.config import on_android_platform, from_service_file
+if on_android_platform() and not from_service_file():
+    from kivymd.toast import toast
+else:
+    def toast(text=None,length_long=0):
+        print(f'Fallback toast - text: {text}, length_long: {length_long}')
