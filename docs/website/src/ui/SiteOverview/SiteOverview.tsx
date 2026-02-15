e54f0e089876ea89f35f96bd7e66119997799235
Fabian <fabianjoseph063@gmail.com>
2026-02-15 01:17:42 +0100

    Update SiteOverview.tsx

--- docs/website/src/ui/SiteOverview/SiteOverview.tsx
+++ docs/website/src/ui/SiteOverview/SiteOverview.tsx
@@ -81,7 +81,7 @@ export default function SiteOverview({ version }: { version: Iversion }) {
 
     async function changeVersionData(version: Iversion) {
 
-        const data = await import(`../../pages/versions-data/${version}.tsx`);
+        // const data = await import(`../../pages/versions-data/${version}.tsx`);
         // console.log(data,' 1p11')
         setData([
     {
