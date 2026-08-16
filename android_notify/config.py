5c7607f1518013806232bb39d485f4b9a6ec8647
Fabian <fabianjoseph063@gmail.com>
2026-08-16 19:11:00 +0100

    optimize lazy imports to keep start speed at 0 (#54)
    * Refactor platform detection and lazy imports- start-up speed issue
    
    * start-up speed issue 1
    
    * cleanup and removed unnessary cache
    
    * incase someone switches activity and no androidx checks on non-android platform
    
    * write cleanup

--- android_notify/config.py
+++ android_notify/config.py
@@ -1,12 +1,32 @@
+"""Environment detection and Android component accessors.
+
+This module provides functions to determine the environment in which the code is running (e.g., Kivy on Android, Flet app, etc.) 
+And to access Android-specific components like the activity, service, notification manager, and storage paths.
+
+It uses lazy initialization to keep the startup time at 0 and avoid errors in non-Android environments.
+"""
+
+
 import os
 
 __version__ = "1.61.6"
 
 from .internal.logger import logger
 
+
+_activity_class_name = None
+
+_jnius = None
+_python_activity = None
+_python_service = None
+_activity_context = None
+_app_storage_path = None
+_package_name = None
+_androidx_dependency = None
+
 def on_kivy_android():
     kivy_build = os.environ.get('KIVY_BUILD', '')
-    if kivy_build in {'android'}:
+    if kivy_build == 'android':
         return True
     elif 'P4A_BOOTSTRAP' in os.environ:
         return True
@@ -21,18 +41,27 @@ def on_flet_app():
 
 
 def on_android_platform():
-    return on_kivy_android() or on_flet_app()
+    return on_kivy_android() or bool(on_flet_app())
 
 
-if on_android_platform():
-    try:
-        from jnius import cast, autoclass
-    except ModuleNotFoundError:
+def _get_jnius():
+    global _jnius
+
+    if _jnius is not None:
+        return _jnius
+
+    if on_android_platform():
+        try:
+            from jnius import cast, autoclass
+        except ModuleNotFoundError:
+            cast = lambda x, y: x
+            autoclass = lambda x: None
+    else:
         cast = lambda x, y: x
         autoclass = lambda x: None
-else:
-    cast = lambda x, y: x
-    autoclass = lambda x: None
+
+    _jnius = (cast, autoclass)
+    return _jnius
 
 
 def on_pydroid_app():
@@ -46,35 +75,55 @@ def on_pydroid_app():
     return False
 
 
+
 def has_androidx_dependency():
-    """Check if androidx dependencies are available"""
+    global _androidx_dependency
+
+    if _androidx_dependency is not None:
+        return _androidx_dependency
+
+    if not on_android_platform():
+        _androidx_dependency = False
+        return _androidx_dependency
+
     try:
-        _=autoclass('androidx.core.app.NotificationCompat')
-        return True
+        _, autoclass = _get_jnius()
+        autoclass("androidx.core.app.NotificationCompat")
+        _androidx_dependency = True
     except Exception:
-        return False
+        _androidx_dependency = False
+
+    return _androidx_dependency
 
 
 def get_activity_class_name():
-    ACTIVITY_CLASS_NAME = os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME")  # flet python
-    if not ACTIVITY_CLASS_NAME:
+    global _activity_class_name
+
+    if _activity_class_name is not None:
+        return _activity_class_name
+
+    activity_class_name = os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME")  # flet python
+
+    if not activity_class_name:
         try:
             # noinspection PyPackageRequirements
             from android import config  # type: ignore
-            ACTIVITY_CLASS_NAME = config.JAVA_NAMESPACE
+            activity_class_name = config.JAVA_NAMESPACE
         except (ImportError, AttributeError):
-            ACTIVITY_CLASS_NAME = 'org.kivy.android'
-    return ACTIVITY_CLASS_NAME
+            activity_class_name = "org.kivy.android"
+
+    _activity_class_name = activity_class_name
+
+    return _activity_class_name
 
 
 def from_service_file():
     return 'PYTHON_SERVICE_ARGUMENT' in os.environ
 
 
-run_on_ui_thread = None
 if on_flet_app() or from_service_file() or not on_android_platform():
     def run_on_ui_thread(func):
-        """Fallback for Developing on PC"""
+        """Fallback for development/non-Kivy environments."""
 
         def wrapper(*args, **kwargs):
             logger.warning("Simulating run on UI thread")
@@ -87,46 +136,73 @@ elif on_kivy_android():
 
 
 def get_python_activity():
+    global _python_activity
+
+    if _python_activity is not None:
+        return _python_activity
+
     if not on_android_platform():
         logger.warning("Can't get python activity, Not on Android.")
         from .internal.facade import PythonActivity
-        return PythonActivity
-    ACTIVITY_CLASS_NAME = get_activity_class_name()
+        _python_activity = PythonActivity
+        return _python_activity
+
+    _, autoclass = _get_jnius()
+    activity_class_name = get_activity_class_name()
+
     if on_flet_app():
-        PythonActivity = autoclass(ACTIVITY_CLASS_NAME)
+        class_name = activity_class_name
     else:
-        PythonActivity = autoclass(ACTIVITY_CLASS_NAME + '.PythonActivity')
-    return PythonActivity
+        class_name = activity_class_name + '.PythonActivity'
+    _python_activity = autoclass(class_name)
+    return _python_activity
+
 
 
 def get_python_service():
+    global _python_service
+
+    if _python_service is not None:
+        return _python_service
+
     if not on_android_platform():
         from .internal.facade import PythonActivity
         logger.warning("Can't get python service, Not on Android.")
-        return PythonActivity
+        _python_service = PythonActivity
+        return _python_service
+
+    _, autoclass = _get_jnius()
     PythonService = autoclass(get_activity_class_name() + '.PythonService')
-    return PythonService.mService
+    _python_service = PythonService.mService
+    return _python_service
 
 
 def get_python_activity_context():
+    global _activity_context
+
+    if _activity_context is not None:
+        return _activity_context
+
     if not on_android_platform():
         logger.warning("Can't get python context, Not on Android.")
         from .internal.facade import Context
-        return Context
+        _activity_context = Context
+        return _activity_context
 
-    PythonActivity = get_python_activity()
     if from_service_file():
         service = get_python_service()
-        context = service.getApplication().getApplicationContext()
+        _activity_context = service.getApplication().getApplicationContext()
     else:
-        context = PythonActivity.mActivity
-    return context
+        PythonActivity = get_python_activity()
+        _activity_context = PythonActivity.mActivity
+    return _activity_context
 
 
 def get_notification_manager():
     if not on_android_platform():
         logger.warning("Can't get notification manager, Not on Android.")
         return None
+    cast, autoclass = _get_jnius()
     NotificationManager = autoclass('android.app.NotificationManager')
 
     context = get_python_activity_context()
@@ -135,16 +211,27 @@ def get_notification_manager():
 
 
 def app_storage_path():
+    global _app_storage_path
+
+    if _app_storage_path is not None:
+        return _app_storage_path
+
     if on_flet_app():
         context = get_python_activity_context()
-        return os.path.join(context.getFilesDir().getAbsolutePath(), 'flet')
+        _app_storage_path = os.path.join(context.getFilesDir().getAbsolutePath(), 'flet')
     elif on_kivy_android():
         # noinspection PyPackageRequirements
         from android.storage import app_storage_path as kivy_app_storage_path  # type: ignore
-        return kivy_app_storage_path()
+        _app_storage_path = kivy_app_storage_path()
     else:
-        return os.getcwd()  # TODO return file main.py path (not android)
+        _app_storage_path = os.getcwd()
+    return _app_storage_path
 
 
 def get_package_name():
-    return get_python_activity_context().getPackageName()  # package.domain + "." + package.name
+    global _package_name
+
+    if _package_name is not None:
+        return _package_name
+    _package_name = get_python_activity_context().getPackageName()
+    return _package_name
