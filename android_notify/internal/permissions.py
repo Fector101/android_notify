f4473062776da4f1714d1ab1f47fb2766a41ca12
Fabian <fabianjoseph063@gmail.com>
2026-09-20 18:24:11 +0100

    remove debug log

--- android_notify/internal/permissions.py
+++ android_notify/internal/permissions.py
@@ -122,7 +122,6 @@ def ask_notification_permission(callback=None, set_requesting_state=None, legacy
 
     def on_permissions_result(_, grants):
         # _ is permissions, note: grants was empty in cli test
-        logger.warning(f"grants---->{grants}")
         execute_callback(callback, grants[0] if len(grants) else True)
         execute_callback(set_requesting_state, False,from_who="package")
 
